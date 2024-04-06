de0a48f026c13842c4524b80d97d25d0da2f53b4
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-04-06 16:42:32 +0800

    Setup DataContext Build Configuration

--- StudentEnrollment.Data/StudentEnrollmentDbContext.cs
+++ StudentEnrollment.Data/StudentEnrollmentDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using StudentEnrollment.Data.Configuration;
 
 namespace StudentEnrollment.Data;
@@ -21,4 +23,22 @@ public class StudentEnrollmentDbContext : IdentityDbContext
     public DbSet<Student> Students { get; set; }
     public DbSet<Course> Courses { get; set; }
     public DbSet<Enrollment> Enrollments { get; set; }
+}
+
+public class StudentEnrollmentDbContextFactory : IDesignTimeDbContextFactory<StudentEnrollmentDbContext>
+{
+    public StudentEnrollmentDbContext CreateDbContext(string[] args)
+    {
+        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<StudentEnrollmentDbContext>();
+        var connectionString = config.GetConnectionString("StudentEnrollmentDbConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+        return new StudentEnrollmentDbContext(optionsBuilder.Options);
+    }
 }
